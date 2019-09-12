2e9487fdecdcfebc7f758ff7abaccf2978dee743
Douglas Veit <s1229604@monmouth.edu>
2019-09-11 20:12:32 -0400

    fun times

--- GoForIt/src/Goodtry.java
+++ GoForIt/src/Goodtry.java
@@ -3,8 +3,8 @@ public class Goodtry {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print(":) Hello");
-		System.out.println(" class");
+		System.out.print(":) God Luck");
+		System.out.println(" have fun");
 
 	}
 
