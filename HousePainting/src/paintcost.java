4371488aa786dcd10b524482a6ae930f9c92f365
Douglas Veit <s1229604@monmouth.edu>
2019-09-16 20:56:52 -0400

    variables created

---
+++ HousePainting/src/paintcost.java
@@ -0,0 +1,29 @@
+
+public class paintcost {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Calculate cost to paint a house
+		 */
+		double houselengthfeet;
+		double houselengthinches;
+		double housewidthfeet;
+		double housewidthinches;
+		double househeightfeet;
+		double hosueheightinches;
+		double windowlengthfeet;
+		double windowlengthinches;
+		double windowwidthfeet;
+		double windowwidthinches;
+		double windownumbers;
+		double doorlengthfeet;
+		double doorlengthinches;
+		double doorwidthfeet;
+		double doorwidthinches;
+		double doornumber;
+		double cost;
+
+	}
+
+}
