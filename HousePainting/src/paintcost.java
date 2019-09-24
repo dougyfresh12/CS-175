d36b42367d196433068a5ca468099406175b270f
Douglas Veit <s1229604@monmouth.edu>
2019-09-23 21:04:29 -0400

    finshed lab

--- HousePainting/src/paintcost.java
+++ HousePainting/src/paintcost.java
@@ -23,7 +23,12 @@ public class paintcost {
 		int doorwidthfeet;
 		int doorwidthinches;
 		int doornumber;
-		int cost;
+		double cost;
+		double squarefeetofwall;
+		double squarefeetpeak;
+		double Sqftwindow;
+		double Sqftdoor;
+		double totalsqft;
 		Scanner in = new Scanner(System.in);
 		System.out.println("Please enter length of wall in feet:");
 		houselengthfeet= in.nextInt();
@@ -31,12 +36,20 @@ public class paintcost {
 		houselengthinches=in.nextInt();
 		System.out.println("Please enter width of wall in feet:");
 		housewidthfeet=in.nextInt();
-		System.out.println("please enter remaining length of wall in inches:");
+		System.out.println("please enter remaining width of wall in inches:");
 		housewidthinches=in.nextInt();
+		squarefeetofwall=((((double)houselengthfeet+houselengthinches/12))*((double)housewidthfeet+housewidthinches/12));
+	
+				
+				
+				
 		System.out.println("please enter height of house");
 		househeightfeet=in.nextInt();
 		System.out.println("please enter remaining height of house in inches:");
 		househeightinches=in.nextInt();
+		squarefeetpeak=((((double)houselengthfeet+houselengthinches/12))*(housewidthfeet+housewidthinches/12)+((double)houselengthfeet+houselengthinches/12)*(((double)househeightfeet+househeightinches/12)-((double)housewidthfeet+housewidthinches/12))/2);
+
+		
 		System.out.println("please enter the number of windows the house has:");
 		windownumbers=in.nextInt();
 		System.out.println("please enter length of windows in feet:");
@@ -47,6 +60,9 @@ public class paintcost {
 		windowwidthfeet=in.nextInt();
 		System.out.println("please enter remaining width of windows in inches:");
 		windowwidthinches=in.nextInt();
+		Sqftwindow=(((double)windowlengthfeet+windowlengthinches/12)*((double)windowwidthfeet+windowwidthinches/12));
+		
+		
 		System.out.println("please state the number of doors on the house:");
 		doornumber=in.nextInt();
 		System.out.println("please enter the length of the doors in feet:");
@@ -57,8 +73,18 @@ public class paintcost {
 		doorwidthfeet=in.nextInt();
 		System.out.println("Please enter the remaining width of the doors in inches:");
 		doorwidthinches=in.nextInt();
+		Sqftdoor=(((double)doorlengthfeet+doorlengthinches/12)*((double)doorwidthfeet+doorwidthinches/12));
+		
 		System.out.println("please state the cost:");
 		cost=in.nextInt();
+		totalsqft=((squarefeetpeak)*2+(squarefeetofwall)*2)-(windownumbers*Sqftwindow)-(doornumber*Sqftdoor);
+		System.out.println("total ="+totalsqft);
+		System.out.println("cost =" +totalsqft*cost);
+		System.out.println("wall =" +squarefeetofwall);
+		System.out.println("peak =" +squarefeetpeak);
+		System.out.println("window =" +Sqftwindow);
+		System.out.println("door =" +Sqftdoor);
+	
 	}
 
 }
