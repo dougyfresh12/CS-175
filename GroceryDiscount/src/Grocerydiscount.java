c59b0ed0f9ba377e4adf1d2e48bec227310c3b6b
Douglas Veit <s1229604@monmouth.edu>
2019-10-23 20:32:51 -0400

    simplified code

--- GroceryDiscount/src/Grocerydiscount.java
+++ GroceryDiscount/src/Grocerydiscount.java
@@ -8,25 +8,30 @@ public class Grocerydiscount {
 		System.out.println("state the cost of your grocieris: ");
 		while(in.hasNextDouble()) {
 		cost=in.nextDouble();
-		if (cost==0) {
-			break;
-		}
-		else if(cost<10.00) {
-			System.out.println("no coupon for you");
-		}
+		if(cost>=10&&cost<=1000) {
+			if(cost<=10) {
+				System.out.println("did not earn a discount");
+			}
 		else if(cost<=60.00) {
-			System.out.printf("you win a discount coupon of %5.2f (8 percent of purcahse)",cost*.08 );
+					System.out.printf("you win a discount coupon of %5.2f (8 percent of purcahse)",cost*.08 );
+				}	
+			 else if(cost<=150.00){
+					System.out.printf("you win a discount coupon of %5.2f (10 percent of purchase)",cost*.10);
+				}
+				else if(cost<=210.00) {
+					System.out.printf("you win a discount coupon of %5.2f (12 percent of purchase)",cost*.12);
+				}
+				else if(cost>210.00) {
+				double cost4=cost*.14;
+					System.out.printf("you win a discount coupon of %5.2f (14 percent of purchase)",cost4);
+				}
+				
 		}
-		else if(cost<=150.00){
-			System.out.printf("you win a discount coupon of %5.2f (10 percent of purchase)",cost*.10);
-		}
-		else if(cost<=210.00) {
-			System.out.printf("you win a discount coupon of %5.2f (12 percent of purchase)",cost*.12);
-		}
-		else if(cost>210.00) {
-		double cost4=cost*.14;
-			System.out.printf("you win a discount coupon of %5.2f (14 percent of purchase)",cost4);
-		}
+
+		
+	      
+		
+		
 		
 		
 	
