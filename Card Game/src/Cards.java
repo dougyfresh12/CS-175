32c74f6c090b95d393760112f79665ccf8904f33
Douglas Veit <s1229604@monmouth.edu>
2019-11-25 20:34:37 -0500

    cards

--- Card Game/src/Cards.java
+++ Card Game/src/Cards.java
@@ -24,9 +24,58 @@ public class Cards {
 			System.out.println("palyer two "+yourHand[i]);
 		}
 		for(int j=0;j<5;j++) {
-			if (myHand[j]>=1&&myHand[j]<=13){countH=countH+1;};
+			if (myHand[j]>=1&&myHand[j]<=13){
+			{countH=countH+1;};
+			countH++;
+			}
+			else if(myHand[j]>=14&&myHand[j]<=26) {
+				countD++;
+			}
+			else if(myHand[j]>=27&&myHand[j]<=39) {
+				countC++;
+			}
+			else if(myHand[j]>=40&&myHand[j]<=52) {
+				countS++;
+			}
+		}
+			if(countH>countD&&countH>countC&&countH>countS)
+					mysuit="Hearts";
+				else if(countD>countH&&countD>countC&&countD>countS)
+					mysuit="Diamonds";
+				else if(countC>countD&&countC>countH&&countC>countS)
+					mysuit="Clubs";
+				else if(countS>countD&&countS>countC&&countS>countH)
+					mysuit="Spades";
+		
+			 card=0;
+			 countH=0;
+			 countD=0;
+			 countC=0;
+			 countS=0;
 			
-			
+		for(int j=0;j<5;j++) {
+			if (yourHand[j]>=1&&yourHand[j]<=13){
+			{countH=countH+1;};
+			countH++;
+			}
+			else if(yourHand[j]>=14&&yourHand[j]<=26) {
+				countD++;
+			}
+			else if(yourHand[j]>=27&&yourHand[j]<=39) {
+				countC++;
+			}
+			else if(yourHand[j]>=40&&yourHand[j]<=52) {
+				countS++;
+			}
+
+			if(countH>countD&&countH>countC&&countH>countS)
+				yoursuit="Hearts";
+			else if(countD>countH&&countD>countC&&countD>countS)
+				yoursuit="Diamonds";
+			else if(countC>countD&&countC>countH&&countC>countS)
+				yoursuit="Clubs";
+			else if(countS>countD&&countS>countC&&countS>countH)
+				yoursuit="Spades";
 		}
 		
 
